ed7f199a8f4a526678724572d8d4cfc3fe9b3b33
orbachkl <orbachkl@gmail.com>
2018-01-26 00:29:52 +0200

    no message

---
+++ WebSite13_1_2018_LinksToAll/WebSite25_1_2018/ShiftColorsCirclesOlympicFlag.js
@@ -0,0 +1,41 @@
+﻿function MoveRight() {
+    var greenCircle = document.getElementsByClassName('greenBorder')[0];
+    console.log(greenCircle);
+    SwapClasses('blueBorder', 'greenBorder');
+    SwapClasses('blackBorder', 'blueBorder');
+    SwapClasses('redBorder', 'blackBorder');
+    SwapClasses('yellowBorder', 'redBorder');
+
+    greenCircle.classList.add('yellowBorder');
+    greenCircle.classList.remove('greenBorder');
+  
+    
+}
+
+
+
+function MoveLeft() {
+    var blueCircle = document.getElementsByClassName('blueBorder')[0];
+    console.log(blueCircle);
+    SwapClasses('greenBorder', 'blueBorder');
+    SwapClasses('yellowBorder', 'greenBorder');
+    SwapClasses('redBorder', 'yellowBorder');
+    SwapClasses('blackBorder', 'redBorder');
+    
+    blueCircle.classList.add('blackBorder');
+    blueCircle.classList.remove('blueBorder');
+   
+  
+
+}
+
+
+function SwapClasses(currentClass,newClass)
+{
+    var CurrentCircle = document.getElementsByClassName(currentClass)[0];
+    CurrentCircle.classList.add(newClass);
+    CurrentCircle.classList.remove(currentClass);
+    
+   
+
+}
