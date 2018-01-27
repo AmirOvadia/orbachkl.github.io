fc62c3520e92e58d2036a58ed6f8ff752efa6ea6
orbachkl <orbachkl@gmail.com>
2018-01-28 01:06:53 +0200

    no message

---
+++ WebSite18_1_2018/ColorAnimationCirclesOlympicFlag.js
@@ -0,0 +1,30 @@
+﻿function ColorBackGround(circleClass) {
+    var circle = document.getElementsByClassName(circleClass)[0];
+    circle.classList.add('dynamic');
+    console.log(circle.classList);
+    console.log(circle.classList[1]);
+
+   
+
+
+    switch (circle.classList[1]) {
+        case 'circle1':
+            circle.classList.toggle('blue');
+            break;
+        case 'circle2':
+            circle.classList.toggle('black');
+            break;
+        case 'circle3':
+            circle.classList.toggle('red');
+            break;
+        case 'circle4':
+            circle.classList.toggle('yellow');
+            break;
+        case 'circle5':
+            circle.classList.toggle('green');
+            break;
+    }
+    circle.classList.toggle('whiteTextColor');
+
+}
+
---
+++ WebSite27_1_2018/MoveAnimation1CirclesOlympicFlag.js
@@ -0,0 +1,21 @@
+﻿function ColorBackGround(circleClass) {
+    var circle = document.getElementsByClassName(circleClass)[0];
+    circle.classList.add('dynamic');
+    console.log(circle.innerText);
+   
+
+    circle.classList.toggle('dynamic2');
+    
+    if (circle.innerText == "move me right!") {
+        console.log("1");
+        circle.innerText = "move me left!";
+    }
+    else {
+        console.log("2");
+        circle.innerText = "move me right!";
+    }
+
+        ;
+
+}
+
